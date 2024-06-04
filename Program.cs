7f9f75b463bf58dcbe61ef2746e01a5b5525316c
Nika623 <157220288+Nika623@users.noreply.github.com>
2024-06-04 23:27:02 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+internal class Program
+
+    private static void Main(string[] args)
+    {
+        string[] array = new string[] {“Hello”, “2”, “world”, “:-)”};
+
+    }
+   string CharArray(string[] array) 
+   {
+     int count = 0;
+     for (int i = 0; i < array1.Length; i++)
+     {
+       new string=v;
+       if (array.Length <=3)
+       {
+        count++
+       }
+     }
+   }
+   
+void ConArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
